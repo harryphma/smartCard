9b062e7f282b8ffa0fdd10c83349b919d104ce6c
harryphma <phamquanghung2705@gmail.com>
2025-01-07 21:18:42 -0500

    add profile icon

---
+++ frontend_gemini/react-card/src/components/Icon.jsx
@@ -0,0 +1,9 @@
+import { FaUser } from 'react-icons/fa';
+
+export default function Icon(){
+    return (
+        <div className='icon-container'>
+            <FaUser size={20} />
+        </div>
+    );
+}
