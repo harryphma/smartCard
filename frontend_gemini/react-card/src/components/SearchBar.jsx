24da53f12743a255bd65c3b354eb5abcc4883ae6
harryphma <phamquanghung2705@gmail.com>
2025-01-07 20:33:49 -0500

    add basic search bar

---
+++ frontend_gemini/react-card/src/components/SearchBar.jsx
@@ -0,0 +1,9 @@
+
+export default function SearchBar() {
+    return (
+        <div className="search-bar">
+            <span className="search-icon">🔍</span>
+            <input type="text" placeholder="Search..." className="search-input"></input>
+        </div>
+    );
+}
