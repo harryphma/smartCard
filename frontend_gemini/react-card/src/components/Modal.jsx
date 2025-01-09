048ec2810cfa86bed67f778b556977b3337f54e7
harryphma <phamquanghung2705@gmail.com>
2025-01-08 21:27:43 -0500

    add modal

---
+++ frontend_gemini/react-card/src/components/Modal.jsx
@@ -0,0 +1,24 @@
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import Button from "./Button.jsx";
+
+const Modal = forwardRef(({buttonCaption, children}, ref) => {
+    const dialog = useRef();
+    useImperativeHandle(ref, () => {
+        return {
+            open(){
+                dialog.current.showModal();
+            }
+        }
+    });
+
+    return (
+        <dialog ref={dialog} className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md">
+            {children}
+            <form method="dialog" className="mt-4 text-right">
+                    <Button>{buttonCaption}</Button>
+            </form>
+        </dialog>
+    );
+});
+
+export default Modal;
--- frontend_gemini/react-card/src/components/NoDeckSelected.jsx
+++ frontend_gemini/react-card/src/components/NoDeckSelected.jsx
@@ -1,12 +1,27 @@
+import { useRef } from "react";
 import Button from "./Button.jsx";
+import Modal from "./Modal.jsx";
 
 export default function NoDeckSelected() {
+
+    const modal = useRef();
+
+    function handleModal(){
+        modal.current.open();
+    }
     return (
-        <div className="w-2/3 h-screen flex flex-col justify-center items-center text-center ml-auto">
-            <h2 className="text-xl font-bold text-stone-500 my-4 whitespace-nowrap">Start Learning</h2>
-            <p className="mt-8">
-                <Button>Create a new deck</Button>
-            </p>
-        </div>
+        <>
+            <Modal ref={modal} buttonCaption = "Close">
+                <h2 className="text-xl font-bold text-stone-700 my-4" >Invalid Input</h2>
+                <p className="text-stone-600 mb-4">Forgot to enter a value.</p>
+                <p className="text-stone-600 mb-4">Make sure to double check</p>
+            </Modal>
+            <div className="w-2/3 h-screen flex flex-col justify-center items-center text-center ml-auto">
+                <h2 className="text-xl font-bold text-stone-500 my-4 whitespace-nowrap">Start Learning</h2>
+                <p className="mt-8">
+                    <Button onClick={handleModal}>Create a new deck</Button>
+                </p>
+            </div>
+        </>
     );
 }
