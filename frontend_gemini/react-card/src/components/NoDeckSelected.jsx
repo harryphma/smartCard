971038a37f6bb8311a260bd7679446c642f35c16
harryphma <phamquanghung2705@gmail.com>
2025-01-10 20:58:36 -0500

    modal + form

--- frontend_gemini/react-card/src/components/NoDeckSelected.jsx
+++ frontend_gemini/react-card/src/components/NoDeckSelected.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Button from "./Button.jsx";
 import Modal from "./Modal.jsx";
 
@@ -9,12 +9,42 @@ export default function NoDeckSelected() {
     function handleModal(){
         modal.current.open();
     }
+    const [nameIsValid, setNameIsValid] = useState(true);
+    const nameDeck = useRef();
+    const numberCards = useRef();
+
+    function handleSubmit(event){
+        event.preventDefault();
+
+        const enteredNameDeck = nameDeck.current.value;
+        const enteredNumberCards = numberCards.current.value;
+
+        if (enteredNameDeck.trim() === ''){
+            setNameIsValid(false);
+            return;
+        }
+
+        setNameIsValid(true);
+    }
+
     return (
         <>
             <Modal ref={modal} buttonCaption = "Generate">
-                <h2 className="text-xl font-bold text-stone-700 my-4" >Invalid Input</h2>
-                <p className="text-stone-600 mb-4">Forgot to enter a value.</p>
-                <p className="text-stone-600 mb-4">Make sure to double check</p>
+                <form onSubmit={handleSubmit}>
+                    <h2>User Input</h2>
+                    <div>
+                        <label htmlFor="nameDeck">Name of the deck</label>
+                        <input type="text" id="nameDeck" ref={nameDeck} />
+                        <div>
+                            {!nameIsValid && <p>Please enter name</p>}
+                        </div>
+
+                        <label htmlFor="numberCards">Number of cards</label>
+                        <input type="number" id="numberCards" ref={numberCards} />
+                    </div>
+                    <button type="submit">Generate</button>
+                </form>
+
             </Modal>
             <div className="w-2/3 h-screen flex flex-col justify-center items-center text-center ml-auto">
                 <h2 className="text-xl font-bold text-stone-500 my-4 whitespace-nowrap">Start Learning</h2>
